ffa9c8ab1de8b9a0fe17ef9327fd0873975db690
Josip Petric <josip@bitforest.eu>
2023-06-15 05:42:51 +0200

    Create the Feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Josip Petric on 15.06.2023..
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
