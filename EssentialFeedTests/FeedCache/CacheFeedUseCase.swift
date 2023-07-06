6a546d932ddd57f005cdb9201e9ecdf170bf559d
Josip Petric <josip@bitforest.eu>
2023-07-06 15:08:08 +0200

    Save command fails on cache deletion error

--- EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
@@ -17,8 +17,9 @@ class LocalFeedLoader {
 		self.currentDate = currentDate
 	}
 
-	func save(_ items: [FeedItem]) {
+	func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
 		store.deleteCachedFeed { [unowned self] error in
+			completion(error)
 			if error == nil {
 				self.store.insert(items, timestamp: currentDate())
 			}
@@ -67,7 +68,7 @@ final class CacheFeedUseCase: XCTestCase {
 		let (sut, store) = makeSUT()
 		
 		let items = [uniqueItem(), uniqueItem()]
-		sut.save(items)
+		sut.save(items) { _ in }
 		
 		XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed])
 	}
@@ -76,7 +77,7 @@ final class CacheFeedUseCase: XCTestCase {
 		let (sut, store) = makeSUT()
 		
 		let items = [uniqueItem(), uniqueItem()]
-		sut.save(items)
+		sut.save(items) { _ in }
 		let deletionError = anyNSError()
 		store.completeDeletion(with: deletionError)
 		
@@ -88,10 +89,29 @@ final class CacheFeedUseCase: XCTestCase {
 		let (sut, store) = makeSUT(currentDate: { timestamp })
 
 		let items = [uniqueItem(), uniqueItem()]
-		sut.save(items)
+		sut.save(items) { _ in }
 		store.completeDeletionSuccessfully()
 		
 		XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed, .insert(items, timestamp)])
+	}
+
+	func test_save_failsOnDeletionError() {
+		let (sut, store) = makeSUT()
+		
+		let items = [uniqueItem(), uniqueItem()]
+		let deletionError = anyNSError()
+		var receivedError: Error?
+		
+		let exp = expectation(description: "Wait for save completion")
+		
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+		store.completeDeletion(with: deletionError)
+		wait(for: [exp], timeout: 1.0)
+		
+		XCTAssertEqual(receivedError as NSError?, deletionError)
 	}
 	
 	// MARK: - Helpers
