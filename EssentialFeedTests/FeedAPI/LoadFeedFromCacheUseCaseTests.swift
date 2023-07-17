7ee7fe1ba7ec1a8d5afb57759a91a40a7ec8db9a
Josip Petric <josip@bitforest.eu>
2023-07-17 15:34:21 +0200

    Extract `FeedStoreSpy` into a shared scope to remove duplication

--- EssentialFeedTests/FeedAPI/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedAPI/LoadFeedFromCacheUseCaseTests.swift
@@ -30,41 +30,4 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
-
-	private class FeedStoreSpy: FeedStore {
-		enum ReceivedMessage: Equatable {
-			case deleteCacheFeed
-			case insert([LocalFeedImage], Date)
-		}
-		
-		private(set) var receivedMessages = [ReceivedMessage]()
-		private var deletionCompletions = [DeletionCompletion]()
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-			deletionCompletions.append(completion)
-			receivedMessages.append(.deleteCacheFeed)
-		}
-		
-		func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-			receivedMessages.append(.insert(feed, timestamp))
-			insertionCompletions.append(completion)
-		}
-		
-		func completeDeletion(with error: Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index: Int = 0) {
-			deletionCompletions[index](nil)
-		}
-		
-		func completeInsertion(with error: Error, at index: Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func completeInsertionSuccessfully(at index: Int = 0) {
-			insertionCompletions[index](nil)
-		}
-	}
 }
