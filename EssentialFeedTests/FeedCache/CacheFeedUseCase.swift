f02b998a247abaf2cb53c7d7d8aa646750268bd4
Josip Petric <josip@bitforest.eu>
2023-07-19 07:19:45 +0200

    Extract duplicate test helpers in the shared scope

--- EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
@@ -128,22 +128,4 @@ final class CacheFeedUseCase: XCTestCase {
 		
 		XCTAssertEqual(receivedError as NSError?, expectedError, file: file, line: line)
 	}
-	
-	private func uniqueImage() -> FeedImage {
-		return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-	}
-	
-	private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-		let feed = [uniqueImage(), uniqueImage()]
-		let localFeed = feed.map({ LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) })
-		return (feed, localFeed)
-	}
-	
-	private func anyURL() -> URL {
-		return URL(string: "http://any-url.com")!
-	}
-
-	private func anyNSError() -> NSError {
-		return NSError(domain: "any error", code: 0)
-	}
 }
---
+++ EssentialFeedTests/FeedCache/Helpers/FeedCacheTestsHelpers.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCacheTestsHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Josip Petric on 19.07.2023..
+//
+
+import Foundation
+import EssentialFeed
+
+func uniqueImage() -> FeedImage {
+	return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+	let feed = [uniqueImage(), uniqueImage()]
+	let localFeed = feed.map({ LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) })
+	return (feed, localFeed)
+}
+
+extension Date {
+	func adding(days: Int) -> Date {
+		return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+	}
+	
+	func adding(seconds: TimeInterval) -> Date {
+		return self + seconds
+	}
+}
