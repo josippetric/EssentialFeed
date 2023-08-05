f17585aa4db4ec97c2fab7849485b022f23b10dc
Josip Petric <josip@bitforest.eu>
2023-08-05 10:33:09 +0200

    Proved that the 'CodableFeedStore' side-effects run serially

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -121,6 +121,32 @@ final class CodableFeedStoreTests: XCTestCase {
 		
 		XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
 	}
+
+	func test_storeSideEffects_runsSerially() {
+		let sut = makeSUT()
+		var completedOperationsOrder = [XCTestExpectation]()
+		
+		let op1 = expectation(description: "operation 1")
+		sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+			completedOperationsOrder.append(op1)
+			op1.fulfill()
+		}
+		
+		let op2 = expectation(description: "operation 2")
+		sut.deleteCachedFeed { _ in
+			completedOperationsOrder.append(op2)
+			op2.fulfill()
+		}
+
+		let op3 = expectation(description: "operation 3")
+		sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
+			completedOperationsOrder.append(op3)
+			op3.fulfill()
+		}
+		
+		waitForExpectations(timeout: 5.0)
+		XCTAssertEqual(completedOperationsOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+	}
 	
 	// MARK: - Helpers
 	
