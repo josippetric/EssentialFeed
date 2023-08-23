5fbf1be546ad138fa9282e8d1af19b7a8b8173ec
Josip Petric <josip@bitforest.eu>
2023-08-23 14:19:22 +0200

    Render loaded feed on successful load completiojn

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Josip Petric on 23.08.2023..
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+}
