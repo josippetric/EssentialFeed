8c4f1f8e5839f6288895ce5e201d50a791c595a8
Josip Petric <josip@bitforest.eu>
2023-08-29 09:46:50 +0200

    Extract 'FeedViewController' creation and configuration into a factory method

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -14,11 +14,9 @@ public final class FeedUIComposer {
 	public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
 		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
 		
-		let bundle = Bundle(for: FeedViewController.self)
-		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-		let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-		feedController.delegate = presentationAdapter
-		feedController.title = FeedPresenter.title
+		let feedController = FeedViewController.makeWith(
+			delegate: presentationAdapter,
+			title: FeedPresenter.title)
 		
 		let presenter = FeedPresenter(
 			feedView: FeedViewAdapter(controller: feedController, loader: imageLoader),
@@ -26,6 +24,17 @@ public final class FeedUIComposer {
 		)
 		presentationAdapter.presenter = presenter
 		
+		return feedController
+	}
+}
+
+private extension FeedViewController {
+	static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+		let bundle = Bundle(for: FeedViewController.self)
+		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+		let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+		feedController.delegate = delegate
+		feedController.title = FeedPresenter.title
 		return feedController
 	}
 }
