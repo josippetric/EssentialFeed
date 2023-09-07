46f2de68a89b3390f3256e4d5bd3a0f47b0874c6
Josip Petric <josip@bitforest.eu>
2023-09-07 06:00:43 +0200

    Synchronize access to global URLProtocolStub.stub with a private `DispatchQueue` to prevent data races

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -157,16 +157,24 @@ final class URLSessionHTTPClientTests: XCTestCase {
 			let data: Data?
 			let response: URLResponse?
 			let error: Error?
+			let requestObserver: ((URLRequest) -> Void)?
 		}
-		private static var stub: Stub?
+		private static var _stub: Stub?
+		private static var stub: Stub? {
+			get { return queue.sync { _stub } }
+			set { queue.sync { _stub = newValue } }
+		}
+		
+		private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
+		
 		private static var requestObserver: ((URLRequest) -> Void)?
 		
 		static func stub(data: Data?, response: URLResponse?, error: Error?) {
-			stub = Stub(data: data, response: response, error: error)
+			stub = Stub(data: data, response: response, error: error, requestObserver: nil)
 		}
 		
 		static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-			requestObserver = observer
+			stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
 		}
 
 		static func startInterceptingRequests() {
@@ -188,10 +196,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
 		}
 		
 		override func startLoading() {
-			if let requestObserver = URLProtocolStub.requestObserver {
-				client?.urlProtocolDidFinishLoading(self)
-				return requestObserver(request)
-			}
 			guard let stub = URLProtocolStub.stub else { return }
 			
 			if let data = stub.data {
@@ -204,8 +208,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
 			
 			if let error = stub.error {
 				client?.urlProtocol(self, didFailWithError: error)
+			} else {
+				client?.urlProtocolDidFinishLoading(self)
 			}
-			client?.urlProtocolDidFinishLoading(self)
+			
+			stub.requestObserver?(request)
 		}
 
 		override func stopLoading() {}
