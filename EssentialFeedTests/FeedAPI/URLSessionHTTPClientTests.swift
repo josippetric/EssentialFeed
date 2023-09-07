7bfd7c02d26739eafdce7cf0e783bd90d200ab89
Josip Petric <josip@bitforest.eu>
2023-09-07 06:07:08 +0200

    Remove default URLSessionHTTPClient initializer parameter (dependency) as clients should be mindful about which `URLSession` instance to use. For example, clients may decide to use the `.shared` instance, but when testing we can inject a `URLSession` with an ephemeral configuration registered with the `URLProtocolStub` class. This way, we don't need to start/stop intercepting URL requests globally.

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -10,14 +10,9 @@ import EssentialFeed
 
 final class URLSessionHTTPClientTests: XCTestCase {
 	
-	override func setUp() {
-		super.setUp()
-		URLProtocolStub.startInterceptingRequests()
-	}
-	
 	override func tearDown() {
 		super.tearDown()
-		URLProtocolStub.stopInterceptingRequests()
+		URLProtocolStub.removeStub()
 	}
 
 	func test_getFromURL_performsGETRequestWithURL() {
@@ -95,7 +90,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
 	// MARK - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
-		let sut = URLSessionHTTPClient()
+		let configuration = URLSessionConfiguration.ephemeral
+		configuration.protocolClasses = [URLProtocolStub.self]
+		let session = URLSession(configuration: configuration)
+
+		let sut = URLSessionHTTPClient(session: session)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}
@@ -167,8 +166,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
 		
 		private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
 		
-		private static var requestObserver: ((URLRequest) -> Void)?
-		
 		static func stub(data: Data?, response: URLResponse?, error: Error?) {
 			stub = Stub(data: data, response: response, error: error, requestObserver: nil)
 		}
@@ -177,14 +174,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
 			stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
 		}
 
-		static func startInterceptingRequests() {
-			URLProtocol.registerClass(URLProtocolStub.self)
-		}
-
-		static func stopInterceptingRequests() {
-			URLProtocol.unregisterClass(URLProtocolStub.self)
+		static func removeStub() {
 			stub = nil
-			requestObserver = nil
 		}
 
 		override class func canInit(with request: URLRequest) -> Bool {
