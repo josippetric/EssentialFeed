b76513b7f9ed316f98ba9905efad178856723606
Josip Petric <josip@bitforest.eu>
2023-09-26 05:16:23 +0200

    Record feed with failed image snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -35,6 +35,14 @@ final class FeedSnapshotTests: XCTestCase {
 		record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
 	}
 	
+	func test_feedWithFailedImageLoading() {
+		let sut = makeSUT()
+		
+		sut.display(feedWithFailedImageLoading())
+		
+		record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT() -> FeedViewController {
@@ -61,6 +69,19 @@ final class FeedSnapshotTests: XCTestCase {
 				location: "Garth Pier",
 				image: UIImage.make(withColor: .green)
 			)
+		]
+	}
+	
+	private func feedWithFailedImageLoading() -> [ImageStub] {
+		return [
+			ImageStub(
+				description: nil,
+				location: "Cannon Street, Londong",
+				image: nil),
+			ImageStub(
+				description: nil,
+				location: "Brighton Seafront",
+				image: nil)
 		]
 	}
 	
@@ -117,7 +138,7 @@ private class ImageStub: FeedImageCellControllerDelegate {
 			location: location,
 			image: image,
 			isLoading: false,
-			shouldRetry: false)
+			shouldRetry: image == nil)
 	}
 	
 	func didRequestImage() {
