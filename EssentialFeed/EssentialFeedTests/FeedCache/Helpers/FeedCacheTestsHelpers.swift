9ce9df5167d33025ede2c14a200837d0fc2381c6
Josip Petric <josip@bitforest.eu>
2023-10-18 14:23:18 +0200

    Adds feed item creation helpers to the Shared test scope to be reused

--- EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestsHelpers.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestsHelpers.swift
@@ -8,16 +8,6 @@
 import Foundation
 import EssentialFeed
 
-func uniqueImage() -> FeedImage {
-	return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-}
-
-func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-	let feed = [uniqueImage(), uniqueImage()]
-	let localFeed = feed.map({ LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) })
-	return (feed, localFeed)
-}
-
 extension Date {
 	func minusFeedCacheMaxAge() -> Date {
 		return adding(days: -feedCacheMaxAgeInDays)
