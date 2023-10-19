f38c014b336a496c5e339d4463797b03841e149b
Josip Petric <josip@bitforest.eu>
2023-10-19 08:12:15 +0200

    Implement image comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Josip Petric on 19.10.2023..
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+	@IBOutlet private(set) public var usernameLabel: UILabel?
+	@IBOutlet private(set) public var messageLabel: UILabel?
+	@IBOutlet private(set) public var dateLabel: UILabel?
+}
