901aee2e9269623ce528d3dc52d6b4265645b7a6
Josip Petric <josip@bitforest.eu>
2023-10-24 06:00:01 +0200

    Duplicate FeedUIComposer as CommentsUIComposer

---
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,47 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Josip Petric on 24.10.2023..
+//
+
+import UIKit
+import Combine
+import EssentialFeed
+import EssentialFeediOS
+
+public final class CommentsUIComposer {
+	private init() {}
+	
+	public static func commentsComposedWith(
+		commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>)
+	-> ListViewController {
+		let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(
+			loader: { commentsLoader().dispatchOnMainQueue() })
+		
+		let feedController = makeFeedViewController(
+			title: FeedPresenter.title )
+		feedController.onRefresh = presentationAdapter.loadResource
+		
+		let presenter = LoadResourcePresenter(
+			resourceView: FeedViewAdapter(
+				controller: feedController,
+				loader: { _ in Empty<Data, Error>().eraseToAnyPublisher() }),
+			loadingView: WeakRefVirtualProxy(object: feedController),
+			errorView: WeakRefVirtualProxy(object: feedController),
+			mapper: FeedPresenter.map
+		)
+		presentationAdapter.presenter = presenter
+		
+		return feedController
+	}
+	
+	private static func makeFeedViewController(title: String) -> ListViewController {
+		let bundle = Bundle(for: ListViewController.self)
+		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+		let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+		feedController.title = FeedPresenter.title
+		return feedController
+	}
+}
+
--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -12,7 +12,7 @@ import EssentialFeed
 import UIKit
 import Combine
 
-final class FeedUIIntegrationTests: XCTestCase {
+class FeedUIIntegrationTests: XCTestCase {
 	
 	func test_feedView_hasTitle() {
 		let (sut, _) = makeSUT()
